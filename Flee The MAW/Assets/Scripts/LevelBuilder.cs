8209302d2d604ba33a8c80626b68c1e34d41255b
Unknown <randomsimpson@gmail.com>
2018-12-23 03:49:11 -0600

    Project Creation
    Project created in Unity.
    First "Ring creation models" made in Blender, then imported to Unity as .fbx file.
    
    Started "Level-building" process. Platforms spawn in a ring around the center of the level. Took a lot of time to troubleshoot some of the problems I was having with it, but it seems good enough for now.
    For now it's just spawning platforms randomly from an array. Next, I need to give it some rules for spawning/not spawning certain platforms in certain places.

---
+++ Flee The MAW/Assets/Scripts/LevelBuilder.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBuilder : MonoBehaviour {
+
+	[SerializeField] GameObject spawner;
+
+	[SerializeField] GameObject[] platforms;
+	[SerializeField] GameObject fullWallPrefab;
+	[SerializeField] GameObject jumpPad;
+	[SerializeField] GameObject nullGap;
+
+	int howManyRingsSpawned = 0;
+
+	void Awake() {
+		for (int i = 1; i <= 4; i++) {
+			transform.position = Vector3.up * (i * 4.5f);
+			SpawnRing();
+		}
+
+	}
+
+
+	void Start () {
+		
+	}
+
+
+	void Update () {
+		
+	}
+
+
+	void SpawnRing() {
+		howManyRingsSpawned++;
+		GameObject ringParent = new GameObject("RingParent" + howManyRingsSpawned);
+		ringParent.transform.position = transform.position;
+
+		for (int i = 0; i <= 300; i += 15) {
+			if (i == 0) {
+				ringParent.transform.Rotate(Vector3.up, 15 * howManyRingsSpawned);
+				GameObject fullWall = Instantiate(fullWallPrefab, spawner.transform.position, Quaternion.identity, ringParent.transform);
+				fullWall.transform.Rotate(-90, 0, 0);
+
+				ringParent.transform.Rotate(Vector3.up, 15);
+				GameObject jumper = Instantiate(jumpPad, spawner.transform.position, Quaternion.identity, ringParent.transform);
+				jumper.transform.Rotate(-90, 0, 0);
+			}
+			else {
+			ringParent.transform.Rotate(Vector3.up, 15);
+
+			int randomPlatform = Random.Range(0, platforms.Length);
+			
+			GameObject newPlatform = Instantiate(platforms[randomPlatform], spawner.transform.position, Quaternion.identity, ringParent.transform);
+			newPlatform.transform.Rotate(-90, 0, 0);
+			}
+
+
+			//Debug.Log(ringParent.transform.rotation.eulerAngles);
+			Debug.Log(spawner.transform.position);
+		}
+
+		ringParent.transform.Rotate(Vector3.up, -15 - (30 * howManyRingsSpawned));
+		
+
+		transform.Rotate(Vector3.up, -transform.rotation.y);
+		spawner.transform.localPosition = Vector3.forward * -8.65f;
+
+	}
+}
