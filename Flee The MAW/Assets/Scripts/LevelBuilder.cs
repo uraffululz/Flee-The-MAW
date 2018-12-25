f3932eaaf6618b64a6ba61fcbcd353e431552cee
Unknown <randomsimpson@gmail.com>
2018-12-25 03:04:24 -0600

    Basic Implementations
    Set up the initial scene (maw, base ring, and player placeholders).
    The LevelBuilder spawns a few sets of ringed platforms above the player's starting position.
    The player can traverse around the rings, moving and jumping. Their rotation is basically working (which took FOR-FUCKING-EVER), but needs to be smoothed.
    The camera tracks the player as they make their way around the rings. This also took a lot of time to figure out, but was actually easy to fix once I realized what the problem was.
    
    Spent about the past 4 hours just trying to get the camera, and the player's rotation correct. Tried a LOT of different things that didn't work. Very annoying, but I'm glad these things are (basically) implemented, as they are a pretty damn big part of the game.

--- Flee The MAW/Assets/Scripts/LevelBuilder.cs
+++ Flee The MAW/Assets/Scripts/LevelBuilder.cs
@@ -7,18 +7,22 @@ public class LevelBuilder : MonoBehaviour {
 	[SerializeField] GameObject spawner;
 
 	[SerializeField] GameObject[] platforms;
+	[SerializeField] GameObject basePlatform;
 	[SerializeField] GameObject fullWallPrefab;
 	[SerializeField] GameObject jumpPad;
 	[SerializeField] GameObject nullGap;
 
+	int recentPlatInt;
+	GameObject recentPlatform = null;
+
 	int howManyRingsSpawned = 0;
+	
 
 	void Awake() {
-		for (int i = 1; i <= 4; i++) {
+		for (int i = 1; i <= 3; i++) {
 			transform.position = Vector3.up * (i * 4.5f);
 			SpawnRing();
 		}
-
 	}
 
 
@@ -28,7 +32,7 @@ public class LevelBuilder : MonoBehaviour {
 
 
 	void Update () {
-		
+
 	}
 
 
@@ -47,25 +51,39 @@ public class LevelBuilder : MonoBehaviour {
 				GameObject jumper = Instantiate(jumpPad, spawner.transform.position, Quaternion.identity, ringParent.transform);
 				jumper.transform.Rotate(-90, 0, 0);
 			}
-			else {
-			ringParent.transform.Rotate(Vector3.up, 15);
-
-			int randomPlatform = Random.Range(0, platforms.Length);
-			
-			GameObject newPlatform = Instantiate(platforms[randomPlatform], spawner.transform.position, Quaternion.identity, ringParent.transform);
-			newPlatform.transform.Rotate(-90, 0, 0);
+			else if (i == 300) {
+				ringParent.transform.Rotate(Vector3.up, 15);
+				GameObject basePlat = Instantiate(basePlatform, spawner.transform.position, Quaternion.identity, ringParent.transform);
+				basePlat.transform.Rotate(-90, 0, 0);
 			}
+			else if (i%2 == 1) {
+				ringParent.transform.Rotate(Vector3.up, 15);
+				GameObject basePlat = Instantiate(basePlatform, spawner.transform.position, Quaternion.identity, ringParent.transform);
+				basePlat.transform.Rotate(-90, 0, 0);
+			}
+			else {
+				ringParent.transform.Rotate(Vector3.up, 15);
 
+				int randomPlatform = Random.Range(0, platforms.Length);
 
+				if (randomPlatform == recentPlatInt) {
+					ringParent.transform.Rotate(Vector3.up, -15);
+					i -= 15;
+
+				}
+				else {
+					recentPlatInt = randomPlatform;
+
+					GameObject newPlatform = Instantiate(platforms[randomPlatform], spawner.transform.position, Quaternion.identity, ringParent.transform);
+					newPlatform.transform.Rotate(-90, 0, 0);
+					recentPlatform = newPlatform;
+				}
+			}
 			//Debug.Log(ringParent.transform.rotation.eulerAngles);
-			Debug.Log(spawner.transform.position);
+			//Debug.Log(spawner.transform.position);
 		}
-
 		ringParent.transform.Rotate(Vector3.up, -15 - (30 * howManyRingsSpawned));
-		
-
 		transform.Rotate(Vector3.up, -transform.rotation.y);
 		spawner.transform.localPosition = Vector3.forward * -8.65f;
-
 	}
 }
---
+++ Flee The MAW/Assets/Watcher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Watcher : MonoBehaviour {
+
+	[SerializeField] GameObject player;
+
+	void Start () {
+		
+	}
+
+
+	void LateUpdate () {
+		//Vector3 findPlayer = Vector3.RotateTowards(transform.forward, player.transform.position, 1, 1);
+		//transform.position = Vector3.up * player.transform.position.y;
+		Vector3 playerAngle = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+		transform.LookAt(playerAngle);
+	}
+}
